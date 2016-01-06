242f1c72d282d833f86874f9b6091c41003b633c
leilawatson <leila@Leilas-MacBook-Pro.local>
2016-01-07 00:06:15 +1000

    Put new item form in modal & some styling
    inserted the form into a modal popup box
    altered the item labels slightly
    updated some css styles

--- views/closet.php
+++ views/closet.php
@@ -28,83 +28,158 @@
 	<h1>Your Closet</h1>
 
 
-	<div class="form-container"> 
+	<!-- ADD New Item Modal -->
+
+	<!-- Button trigger modal -->
+		<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#newItemModal">
+		  Add a new item
+		</button>
+
+	<!-- Modal -->
+		<div class="modal fade" id="newItemModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <h4 class="modal-title" id="myModalLabel">Add new item</h4>
+		      </div>
+		      <form action="insert.php" method="post" name="addnewitem" enctype="multipart/form-data" id="newItemForm">
+		      <div class="modal-body">
+		        
+			        <p>
+			        <label for="Type">Item Type:</label>
+				        <select name="Type" id="Type" >
+						  <option name="Tank Tops" value="Tank Tops">Tank Tops</option>
+		                  <option name="Short Sleeve Tops" value="Short Sleeve Tops">Short Sleeve Tops</option>
+						  <option name="Long Sleeve Tops" value="Long Sleeve Tops">Long Sleeve Tops</option>
+		                  <option name="Dresses" value="Dresses">Dresses</option>
+		                  <option name="Skirts" value="Skirts">Skirts</option>
+						  <option name="Shorts" value="Shorts">Shorts</option>
+						  <option name="Pants" value="Pants">Pants</option> 
+						</select>
+			        </p>
+		            <p>
+				        <label for="Material">Item Material:</label>
+				        <input type="text" name="Material" id="Material">
+			        </p>
+			        <p>
+				        <label for="Price">Item Price:</label>
+				        <input type="text" name="Price" id="Price">
+			        </p>
+		            <p>
+				        <label for="Description">Item Description:</label>
+		                <textarea name="Description" id="Description"></textarea>
+			        </p>
+		            <p>
+		                <label for="Colour">Item Colour:</label>
+		                <!-- <input type="text" name="Colour" id="Colour">-->
+		                <select name="Colour" id="Colour" >
+		                    <option name="Multi" value="Multi">Multi</option>
+		                    <option name="Black" value="Black">Black</option>
+		                    <option name="Gray" value="Gray">Gray</option>
+		                    <option name="White" value="White">White</option>
+		                    <option name="Red" value="Red">Red</option>
+		                    <option name="Orange" value="Orange">Orange</option>
+		                    <option name="Yellow" value="Yellow">Yellow</option> 
+		                    <option name="Green" value="Green">Green</option>
+		                    <option name="Blue" value="Blue">Blue</option>
+		                    <option name="Purple" value="Purple">Purple</option>
+		                    <option name="Pink" value="Pink">Pink</option>
+		                    <option name="Brown" value="Brown">Brown</option>
+						</select>
+		            </p>
+		            <p>
+		                <label for="uploadimage">Upload Image:</label>
+		                <input name="uploadimage" type="file">
+		            </p>
+			         
+			        <input type="submit" id="newItemSubmit" class="btn btn-primary" value="Add New Item" data-dismiss="modal">
+		        
+		      </div>
+		      </form>
+
+		    </div> <!-- end modalContent -->
+		  </div> <!-- end modalDialog -->
+		</div> <!-- end my modal -->
+
+
+
+	<!-- <div class="form-container"> 
 		<h2>Add new item</h2>
         <form action="insert.php" method="post" name="addnewitem" enctype="multipart/form-data">
         
-        <div class="col-xs-12 col-sm-6 col-md-4">
+	        <div class="col-xs-12 col-sm-6 col-md-4">
 
-	        <p>
-	        <label for="Type">Item Type:</label>
-	        <!-- <input type="text" name="itemtype"> -->
-		        <select name="Type" id="Type" >
-				  <option name="Tank Tops" value="Tank Tops">Tank Tops</option>
-                  <option name="Short Sleeve Tops" value="Short Sleeve Tops">Short Sleeve Tops</option>
-				  <option name="Long Sleeve Tops" value="Long Sleeve Tops">Long Sleeve Tops</option>
-                  <option name="Dresses" value="Dresses">Dresses</option>
-                  <option name="Skirts" value="Skirts">Skirts</option>
-				  <option name="Shorts" value="Shorts">Shorts</option>
-				  <option name="Pants" value="Pants">Pants</option> 
-				</select>
-	        </p>
-            
-            <p>
-		        <label for="Material">Item Material:</label>
-		        <input type="text" name="Material" id="Material">
-	        </p>
-            
-        </div>
+		        <p>
+		        <label for="Type">Item Type:</label>
+			        <select name="Type" id="Type" >
+					  <option name="Tank Tops" value="Tank Tops">Tank Tops</option>
+	                  <option name="Short Sleeve Tops" value="Short Sleeve Tops">Short Sleeve Tops</option>
+					  <option name="Long Sleeve Tops" value="Long Sleeve Tops">Long Sleeve Tops</option>
+	                  <option name="Dresses" value="Dresses">Dresses</option>
+	                  <option name="Skirts" value="Skirts">Skirts</option>
+					  <option name="Shorts" value="Shorts">Shorts</option>
+					  <option name="Pants" value="Pants">Pants</option> 
+					</select>
+		        </p>
+	            
+	            <p>
+			        <label for="Material">Item Material:</label>
+			        <input type="text" name="Material" id="Material">
+		        </p>
+	            
+	        </div>
         
-        <div class="col-xs-12 col-sm-6 col-md-4">
-	        <p>
-		        <label for="Price">Item Price:</label>
-		        <input type="text" name="Price" id="Price">
-	        </p>
-            
-            <p>
-		        <label for="Description">Item Description:</label>
-                <textarea name="Description" id="Description"></textarea>
-	        </p>
+	        <div class="col-xs-12 col-sm-6 col-md-4">
+		        <p>
+			        <label for="Price">Item Price:</label>
+			        <input type="text" name="Price" id="Price">
+		        </p>
+	            
+	            <p>
+			        <label for="Description">Item Description:</label>
+	                <textarea name="Description" id="Description"></textarea>
+		        </p>
 
-        </div>
+	        </div>
         
-        <div class="col-xs-12 col-sm-6 col-md-4">
+	        <div class="col-xs-12 col-sm-6 col-md-4">
 
-            <p>
-                <label for="Colour">Item Colour:</label>
-                <!-- <input type="text" name="Colour" id="Colour">-->
-                <select name="Colour" id="Colour" >
-                    <option name="Multi" value="Multi">Multi</option>
-                    <option name="Black" value="Black">Black</option>
-                    <option name="Gray" value="Gray">Gray</option>
-                    <option name="White" value="White">White</option>
-                    <option name="Red" value="Red">Red</option>
-                    <option name="Orange" value="Orange">Orange</option>
-                    <option name="Yellow" value="Yellow">Yellow</option> 
-                    <option name="Green" value="Green">Green</option>
-                    <option name="Blue" value="Blue">Blue</option>
-                    <option name="Purple" value="Purple">Purple</option>
-                    <option name="Pink" value="Pink">Pink</option>
-                    <option name="Brown" value="Brown">Brown</option>
-				</select>
-            </p>
-            
-            <p>
-                <label for="uploadimage">Upload Image:</label>
-                <input name="uploadimage" type="file">
-            
-            </p>
-            
-        </div>
-            
-        </div>
+	            <p>
+	                <label for="Colour">Item Colour:</label>
+	                <select name="Colour" id="Colour" >
+	                    <option name="Multi" value="Multi">Multi</option>
+	                    <option name="Black" value="Black">Black</option>
+	                    <option name="Gray" value="Gray">Gray</option>
+	                    <option name="White" value="White">White</option>
+	                    <option name="Red" value="Red">Red</option>
+	                    <option name="Orange" value="Orange">Orange</option>
+	                    <option name="Yellow" value="Yellow">Yellow</option> 
+	                    <option name="Green" value="Green">Green</option>
+	                    <option name="Blue" value="Blue">Blue</option>
+	                    <option name="Purple" value="Purple">Purple</option>
+	                    <option name="Pink" value="Pink">Pink</option>
+	                    <option name="Brown" value="Brown">Brown</option>
+					</select>
+	            </p>
+	            
+	            <p>
+	                <label for="uploadimage">Upload Image:</label>
+	                <input name="uploadimage" type="file">
+	            </p>
+	            
+	        </div>
 
-	    <div class="clearfix"></div>
-        	<input type="submit" value="Add New Item">
-
+	        <input type="submit" value="Add New Item">
         </form>
 
+    </div> -->
+
+        
+
 	<div>
+
 	<?php 
 		echo "<h2>Total number of items: ";
 		echo count($items);
@@ -116,25 +191,25 @@
 
 	        foreach ($items as $item) {
 	        	if ($item["Type"] == "Tank Tops") {
-	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 tops'> ";
+	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 tank_tops'> ";
 	        		}
 	        	if ($item["Type"] == "Short Sleeve Tops") {
-	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 bottoms'> ";
+	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 short_sleeves_top'> ";
 	        		}
 	        	if ($item["Type"] == "Long Sleeve Tops") {
-	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 outerwear'> ";
+	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 long_sleeves_top'> ";
 	        		}
 	        	if ($item["Type"] == "Dresses") {
 	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 dresses'> ";
 	        		}
 	        	if ($item["Type"] == "Skirts") {
-	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 short_sleeves_top'> ";
+	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 skirts'> ";
 	        		}
 	        	if ($item["Type"] == "Shorts") {
-	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 long_sleeves_top'> ";
+	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 shorts'> ";
                     }
                 if ($item["Type"] == "Pants") {
-	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 dresses'> ";
+	        		echo "<div class='item-box col-xs-6 col-sm-4 col-md-3 pants'> ";
 	        		}
                 
                     echo "<p>";
@@ -142,39 +217,62 @@
 	        		echo "</p>";
 
 
-	        		echo "<p>Item Type: ";
+	        		echo "<p> <span class='item_label'>Type: </span>";
 	        			echo $item["Type"];
 	        		echo "</p>";
                 
-                    echo "<p>Item Price: ";
+                    echo "<p> <span class='item_label'>Price: </span>";
                     	echo "$ ".number_format($item["Price"], 2);
 	        		echo "</p>";
                 
-                    echo "<p>Cost Per Wear: ";
+                    echo "<p> <span class='item_label'>Cost Per Wear: </span>";
                     	echo "$ ".number_format($item["Price"] / $item["Times_Worn"], 2);
 	        		echo "</p>";
                 
                 
-                    echo "<p>Item Colour: ";
+                    echo "<p> <span class='item_label'>Colour: </span>";
 	        			echo $item["Colour"];
 	        		echo "</p>";
                 
-                    echo "<p>Number of Times Worn: ";
+                    echo "<p><span class='item_label'>Times Worn: </span>";
 	                   echo $item["Times_Worn"];
 	        		echo "</p>";
                 
-                    echo "<p>Number of Days Ago Last Worn: ";
-	                   echo $item["Days_Last_Worn"];
+                    echo "<p> <span class='item_label'>Last Worn: </span>";
+                    	if($item["Days_Last_Worn"] == 1) {
+                    		echo $item["Days_Last_Worn"]." day ago";
+                    	}
+                    	else {
+                    		echo $item["Days_Last_Worn"]." days ago";
+                    	}
+	                   
 	        		echo "</p>";
                 
                 echo "</div>";
 
-
 	         }
-
-
 	            
         ?>
+
+        <!-- Test items for testing styling on local computer -->
+	        <!-- <div class='item-box col-xs-6 col-sm-4 col-md-3 tank_tops'>
+	        	<p><img src=""></p>
+	        	<p>Item Type: Tanks Tops</p>
+				<p>Item Price: $50</p>
+				<p>Cost Per Wear: $4.90</p>
+                <p>Item Colour: Blue</p>
+				<p>Number of Times Worn: 45 </p>
+                <p>Number of Days Ago Last Worn: 3</p>
+	        </div>
+	        <div class='item-box col-xs-6 col-sm-4 col-md-3 shorts'>
+	        	<p><img src=""></p>
+	        	<p>Item Type: Shorts</p>
+				<p>Item Price: $50</p>
+				<p>Cost Per Wear: $4.90</p>
+                <p>Item Colour: Blue</p>
+				<p>Number of Times Worn: 45 </p>
+                <p>Number of Days Ago Last Worn: 3</p>
+	        </div> -->
 
 
 		</div> <!-- end items container -->
@@ -186,8 +284,26 @@
 
 
 <script type="text/javascript">
-	  $(function() {
-	    $( "#datepicker" ).datepicker({dateFormat: 'yy-mm-dd'});
-	  });
-	 </script>
+	  // $(function() {
+	  //   $( "#datepicker" ).datepicker({dateFormat: 'yy-mm-dd'});
+	  // });
+
+	$("#newItemModal").on('hide', function () {
+        window.location.reload();
+	});
+
+	// submits modal form - fixes form submission problem, tutorial from:
+	//http://stackoverflow.com/questions/9349142/twitter-bootstrap-2-modal-form-dialogs/9349329#9349329
+
+	$('#newItemSubmit').on('click', function(e){
+	    // We don't want this to act as a link so cancel the link action
+	    e.preventDefault();
+
+	    // Find form and submit it
+	    $('#newItemForm').submit();
+	});
+
+</script>
+
+
 </html>
