1c2db010e921f0e20741d7b6280c5f726625d201
leilawatson <leila@Leilas-MacBook-Pro.local>
2016-01-10 14:09:01 +1000

    Updated Light Display query
    pete’s file updated

--- query_for_the_light_display_pete.php
+++ query_for_the_light_display_pete.php
@@ -4,43 +4,40 @@ require_once 'utils.php';
 
 $light_colours = "";
 
-$sql = "SELECT Item_ID, Date_Last_Worn FROM Item";
+$sql = "SELECT Item_ID, Date_Last_Worn FROM Item WHERE RFID_Tag != 0";
 $stmt = $db->prepare($sql);
 $stmt->execute();
 $result = $stmt->fetchAll(); //push the result in array
 
 $date1 = new DateTime();
 
-foreach ($result as $row) //loop every single element in results
+foreach ($result as $row) 
+//loop every single element in results
 {
 	$date2 = new DateTime(date('Y-m-d', strtotime($row['Date_Last_Worn']))); //take the heading and read the value from the collumn
 	$daysago = $date1->diff($date2)->days; //calculate the days difference
 	
-	if ($daysago == 0)
+	if ($daysago < 10)
 	{
-		$light_colours .= $row['Item_ID'].":green";
-	}
-	else if ($daysago < 7)
-	{
-		$light_colours .= $row['Item_ID'].":blue";
-	}
-	else if ($daysago < 14)
-	{
-		$light_colours .= $row['Item_ID'].":purple";
+		$light_colours .= "white";
 	}
 	else if ($daysago < 30)
 	{
-		$light_colours .= $row['Item_ID'].":blue";
+		$light_colours .= "light_yellow";
+	}
+	else if ($daysago < 60)
+	{
+		$light_colours .= "yellow";
 	}
 	else if ($daysago < 100)
 	{
-		$light_colours .= $row['Item_ID'].":orange";
+		$light_colours .= "orange";
 	}
 	else
 	{
-		$light_colours .= $row['Item_ID'].":red";
+		$light_colours .= "red";
 	}
-	$light_colours .= "-";
+	$light_colours .= ":";
 }
 
 echo $light_colours;
