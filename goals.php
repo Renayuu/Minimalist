3e8c432d5da6781e21a990e87d268b1a61b0b831
alyssadisalvo <alyssadisalvo@d-i89-145-228.student.eduroam.uq.edu.au>
2016-01-21 15:26:51 +1000

    changes

--- goals.php
+++ goals.php
@@ -5,5 +5,25 @@
 <div class="container">
 
 	<h1 align = "center">GOALS</h1>
+	<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#newItemModal">
+		  edit your goal
+	</button>
+	<h3>
+            You own <span id="no_items"></span> items.
+	</h3>
+
+	<br> 
+	<br> 
+
+	<h3>
+            You want to own XXXXX or less items.
+	</h3>
+
+	<br> 
+	<br> 
+
+	<h3>
+            You have XXXXX items left to reach your goal.
+	</h3>
 
 </div>
