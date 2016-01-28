656a9dfeafc253971f08b119aafc3ee33fb10bc1
leilawatson <leila@s4385140.vpn.uq.edu.au>
2016-01-28 23:23:41 +1000

    Fixed Goals query
    Made a mistake and was retrieving the goal number instead of the
    clothes left number - fixed now :)

--- query_for_goals.php
+++ query_for_goals.php
@@ -5,8 +5,16 @@ require_once 'utils.php';
 $stmt = $db->prepare("SELECT Goal_Num FROM Goal WHERE Goal_ID = 1");
 $stmt->execute();
 $result = $stmt->fetch(); 
-$goal = $result["Goal_Num"];
+$goal = intval($result["Goal_Num"]);
 
-echo $goal;
+$stmt2 = $db->prepare("SELECT COUNT(*) as total FROM Item WHERE Discarded = 0");
+$stmt2->execute();
+$result2 = $stmt2->fetch(); 
+
+$num_clothes = intval($result2['total']);
+
+$clothes_left = $num_clothes - $goal;
+
+echo $clothes_left;
 
 ?>
