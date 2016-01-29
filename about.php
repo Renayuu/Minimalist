6052bbf1de0216816edb1aa73f0d626c18afe4c0
alyssadisalvo <alyssadisalvo@Alyssa-DiSalvos-MacBook-Pro.local>
2016-01-29 19:47:14 +1000

    condensed about

--- about.php
+++ about.php
@@ -12,30 +12,21 @@ $image_url2=''
 	<h1 align = "center">About Minimize</h1>
 
 	<div class="container">
-		<p>Minimize is an Internet of Things project that is primarily intended to assist users in minimizing their wardrobe. This project was imagined and executed by Alyssa DiSalvo, Leila Watson, and Rena Yu (with great help from Pete Worthy) as part of the course DECO7000 Designing Connected Realities at the University of Queensland.</p>
+		<p>Minimize is an Internet of Things project designed to assist users in minimizing their wardrobe. This project was imagined and executed by Alyssa DiSalvo, Leila Watson, and Rena Yu (with great help from Pete Worthy) as part of the course DECO7000 Designing Connected Realities at the University of Queensland.</p>
 		
-		<p>Minimize is designed for a future in which information is already embedded (via RFID tags) within clothing items by manufacturers at the point of production--a future in which clothing items could automatically be cataloged to your account upon their purchase. </p>
+		<p>Minimize is intended for a future in which information is already embedded (via RFID tags) within clothing items by manufacturers at the point of production--a future in which clothing items could automatically be cataloged to your account upon their purchase. </p>
 
 		<p>Our goal with this project is to help users generate greater awareness about their clothing habits, to instigate behavioral and attitudinal change in regards to minimizing, and to encourage users to make more informed decisions regarding which items to keep and which items to part with.</p>
 
 
-		<p>Similar projects and inspirations for Minimize include:</p>
+		<p>Inspirations for Minimize include:</p>
 		<ul>
 			<li><a href="http://www.theminimalists.com">The Minimalists</a></li>
-			<li><a href="http://mnmlist.com/archive/">mnmlist</a></li>
 			<li><a href="http://own-it.us/html/about.html">Mary Mattingly&#39;s OWN-IT.US</a>, <a href="https://www.youtube.com/watch?v=gujH5oYmHcY">Video</a></li>
 			<li><a href="https://www.artangel.org.uk/project/break-down/">Michael Landy&#39;s Break Down</a></li>
 			<li><a href="https://www.youtube.com/watch?v=z20CjCim8DM">Patagonia&#39;s Worn Wear: a Film About the Stories We Wear</a></li>
 			<li><a href="http://significantobjects.com">Significant Objects</a></li>
-			<li><a href="https://objectethnography.wordpress.com/how-it-works/">The Object Ethnography Project</a></li>
-			<li><a href="http://talesofthings.com">Tales of Things</a></li>
-			<li><a href="http://chrisspeed.net/?page_id=351#">RememberMe</a></li>
-			<li><a href="http://shelflife.oxfam.org.uk">Oxfam ShelfLife</a>, <a href="http://chrisspeed.net/?p=773">Link 2</a>, <a href="https://www.youtube.com/watch?v=l897sK8rSe0">Video</a></li>
-			<li><a href="http://pensieve.cornellhci.org">Pensieve</a></li>
-			<li><a href="http://ns.hcilab.jp/~kono/documents/MU3I_kono.031208.pdf">Ubiquitous Memories and I'm Here!</a></li>
-			<li><a href="http://download.springer.com/static/pdf/426/art%253A10.1007%252FBF02391566.pdf?originUrl=http%3A%2F%2Flink.springer.com%2Farticle%2F10.1007%2FBF02391566&token2=exp=1453963957~acl=%2Fstatic%2Fpdf%2F426%2Fart%25253A10.1007%25252FBF02391566.pdf%3ForiginUrl%3Dhttp%253A%252F%252Flink.springer.com%252Farticle%252F10.1007%252FBF02391566*~hmac=f11de1735f6753d29f660e0a3c535c15973e167b6c1ad4bb4eb398334663d254">The Memory Box</a></li>
-			<li><a href="http://download.springer.com/static/pdf/707/art%253A10.1007%252Fs00779-003-0220-4.pdf?originUrl=http%3A%2F%2Flink.springer.com%2Farticle%2F10.1007%2Fs00779-003-0220-4&token2=exp=1453964070~acl=%2Fstatic%2Fpdf%2F707%2Fart%25253A10.1007%25252Fs00779-003-0220-4.pdf%3ForiginUrl%3Dhttp%253A%252F%252Flink.springer.com%252Farticle%252F10.1007%252Fs00779-003-0220-4*~hmac=e228a5158664660f2741d9ba346cd7027aa1979a9fe02807ee5222152348b998">The Living Memory Box</a></li>
-			<li><a href="http://sydney.edu.au/engineering/it/~judy/Homec/Pubs/2006_memento.pdf">Memento</a></li>
+			<li><a href="http://shelflife.oxfam.org.uk">Oxfam ShelfLife</a>, <a href="https://www.youtube.com/watch?v=l897sK8rSe0">Video</a></li>
 		</ul>
 
 	</font></div>
