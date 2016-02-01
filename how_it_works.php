f5dc96106d46233aaef6c7dbd3912c766f9fbcf8
alyssadisalvo <alyssadisalvo@d-i89-145-228.student.eduroam.uq.edu.au>
2016-02-01 16:41:05 +1000

    how it works update

--- how_it_works.php
+++ how_it_works.php
@@ -13,23 +13,25 @@ $image_url2=''
 
 	<div class="container">
 
-		<p>This website is meant to be considered in conjunction with our physical mini-closet prototype (pictured above). As of now, our display allows users to visualize items they are neglecting by featuring light over each item in a color that corresponds with the number of days ago the item was last worn.</p>
 
+		<p>This website is meant to be considered in conjunction with our physical mini-closet prototype and StoryLamp prototype.</p> 
+		<h2><b>Closet Inside</b></h2>
+		<p>There are LED lights above each item. These lights allow users to individually visualize two sets of data:</p>
+		<p/><b>The Number of Days Ago the Item was Last Worn.</b> Our display allows users to visualize items they are neglecting by featuring light over each item in a color that corresponds with the number of days ago the item was last worn. Scanning an item to the RFID reader (which users are instructed to do before wearing the item) will increment the number of times the item was worn and reset the number of days ago it was last worn (setting the light color above the item to white. Visualizing this data can aid users in easily recognizing the items they have neglected, which can prompt them to either use those items more or get rid of them so that they can be of use to someone else.</p>
+		<p><b>The Appropriateness of the Item Given the Current Weather.</b> This can help users identify items to temporarily store away when they are not in season. (This is very similar to the concept of "capsule wardrobes.")</p>
 		<font align = "center">
 		<img class="displayed" src="images/closet.jpg" align="middle">
 		</font>
-
 		<br>
-
-		<p>Scanning an item to the RFID reader will increment the number of times the item was worn and reset the number of days ago it was last worn (setting the light color above the item to white, which signifies that it was worn within the past 10 days). Visualizing this data can aid users in easily recognizing the items they have neglected, which can prompt them to either use those items more or get rid of them so that they can be of use to someone else.</p>
-
+		<h2><b>Closet Doors</b></h2>
+		<h2><b>StoryLamp</b></h2>
+		<p>The purpose of StoryLamp is to assist users in parting with unused sentimental clothing items. Some early feedback we received on our project indicated that even seeing the extent to which items were neglected may not necessarily help users minimize if they experience attachment to their clothing items (which is particularly common among hoarders). StoryLamp was built in response to this feedback.</p>
 		<font align = "center">
-		<img class="displayed" src="images/colors.jpg" align="middle">
+		<img class="displayed" src="images/storylamp.jpg" align="middle">
 		</font>
-
 		<br>
-
-		<p>TO BE ADDED: descriptions of new IoT features we build</p>
+		<p>As Joshua Fields Millburn of The Minimalists writes, "An item that is <i>sentimental</i> for us can be <i>useful</i> for someone else."</p>
+		<p>StoryLamp is designed to feature dynamic images associated with the root of the discarded items&#39; sentimentality, which enables users to preserve memories and retain the identity those items bestowed upon them. RFID technology enables the lamp to glow when one of the user&#39;s donated items is being worn, positively reinforcing the user&#39;s decision to part with the item.</p>
 
 		
 
