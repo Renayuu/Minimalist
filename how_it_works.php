072050178d78c025ae9bbcf71510216937a3bad2
alyssadisalvo <alyssadisalvo@d-i89-145-228.student.eduroam.uq.edu.au>
2016-02-02 09:43:36 +1000

    wording correction

--- how_it_works.php
+++ how_it_works.php
@@ -17,7 +17,7 @@ $image_url2=''
 		<p>This website is meant to be considered in conjunction with our physical mini-closet prototype and StoryLamp prototype.</p> 
 		<h2><b>Closet Inside</b></h2>
 		<p>There are LED lights above each item. These lights allow users to individually visualize two sets of data:</p>
-		<p/><b>The Number of Days Ago the Item was Last Worn.</b> Our display allows users to visualize items they are neglecting by featuring light over each item in a color that corresponds with the number of days ago the item was last worn. Scanning an item to the RFID reader (which users are instructed to do before wearing the item) will increment the number of times the item was worn and reset the number of days ago it was last worn (setting the light color above the item to white. Visualizing this data can aid users in easily recognizing the items they have neglected, which can prompt them to either use those items more or get rid of them so that they can be of use to someone else.</p>
+		<p/><b>The Number of Days Ago the Item was Last Worn.</b> Our display allows users to visualize items they are neglecting by featuring light over each item in a color that corresponds with the number of days ago the item was last worn. Scanning an item to the RFID reader (which users are instructed to do before wearing the item) will increment the number of times the item was worn and reset the number of days ago it was last worn (setting the light color above the item to white). Visualizing this data can aid users in easily recognizing the items they have neglected, which can prompt them to either use those items more or get rid of them so that they can be of use to someone else.</p>
 		<p><b>The Appropriateness of the Item Given the Current Weather.</b> This can help users identify items to temporarily store away when they are not in season. (This is very similar to the concept of "capsule wardrobes.")</p>
 		<font align = "center">
 		<img class="displayed" src="images/closet.jpg" align="middle">
@@ -25,7 +25,7 @@ $image_url2=''
 		<br>
 		<h2><b>Closet Doors</b></h2>
 		<h2><b>StoryLamp</b></h2>
-		<p>The purpose of StoryLamp is to assist users in parting with unused sentimental clothing items. Some early feedback we received on our project indicated that even seeing the extent to which items were neglected may not necessarily help users minimize if they experience attachment to their clothing items (which is particularly common among hoarders). StoryLamp was built in response to this feedback.</p>
+		<p>The purpose of StoryLamp is to assist users in parting with unused sentimental clothing items. Some early feedback we received on our project indicated that even seeing the extent to which items have been neglected may not necessarily help users minimize if they experience attachment to their clothing items (which is particularly common among hoarders). StoryLamp was built in response to this feedback.</p>
 		<font align = "center">
 		<img class="displayed" src="images/storylamp.jpg" align="middle">
 		</font>
