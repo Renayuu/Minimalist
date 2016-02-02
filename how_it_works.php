77347ab2b3edab19baa2ab2902faa6c60a481367
alyssadisalvo <alyssadisalvo@d-i89-145-228.student.eduroam.uq.edu.au>
2016-02-02 15:52:10 +1000

    minor edits

--- how_it_works.php
+++ how_it_works.php
@@ -17,27 +17,32 @@ $image_url2=''
 		<p>This website is meant to be considered in conjunction with our physical mini-closet prototype and StoryLamp prototype.</p> 
 		<h2><b>Closet Inside</b></h2>
 		<p>There are LED lights above each item. These lights allow users to individually visualize two sets of data:</p>
-		<p/><b>The Number of Days Ago the Item was Last Worn.</b> Our display allows users to visualize items they are neglecting by featuring light over each item in a color that corresponds with the number of days ago the item was last worn. Scanning an item to the RFID reader (which users are instructed to do before wearing the item) will increment the number of times the item was worn and reset the number of days ago it was last worn (setting the light color above the item to white). Visualizing this data can aid users in easily recognizing the items they have neglected, which can prompt them to either use those items more or get rid of them so that they can be of use to someone else.</p>
+		<p><b>The Number of Days Ago the Item was Last Worn.</b> Our display allows users to visualize items they are neglecting by featuring light over each item in a color that corresponds with the number of days ago the item was last worn. Scanning an item to the RFID reader (which users are instructed to do before wearing the item) will increment the number of times the item was worn and reset the number of days ago it was last worn (setting the light color above the item to white). Visualizing this data can aid users in easily recognizing the items they have neglected, which can prompt them to either use those items more or get rid of them so that they can be of use to someone else.</p>
 		<p><b>The Appropriateness of the Item Given the Current Weather.</b> This can help users identify items to temporarily store away when they are not in season. (This is very similar to the concept of "capsule wardrobes.")</p>
+		<br>
 		<font align = "center">
 		<img class="displayed" src="images/closet.jpg" align="middle">
 		</font>
 		<br>
+		<br>
 		<h2><b>Closet Doors</b></h2>
-		<p><b>Goal Progress. </b>Visualised on the door of the closet will be a light representing the progress of the user's goal they they can set in the website. </p>
-		<p><b>Discarded Items. </b>The closet doors will also display images of items that the user hasn't worn in over 100 days, with the number of days since it was last worn. This serves as a constant visual reminder to users to encourage them to minimize.</p>
-
+		<p><b>Goal Progress. </b>On the closet door is a NeoPixel ring that allows users to visualize their progress towards meeting the numeric goal they set on this website.</p>
+		<p><b>Discarded Items. </b>The closet doors will also display images of items that the user hasn't worn in over 100 days in addition to the number of days ago since those items were last worn. This serves as a constant visual reminder to encourage users to minimize.</p>
+		<br>
 		<img class="displayed" src="images/doors.jpg" align="middle">
+		<br>
 		<br>
 
 		<h2><b>StoryLamp</b></h2>
 		<p>The purpose of StoryLamp is to assist users in parting with unused sentimental clothing items. Some early feedback we received on our project indicated that even seeing the extent to which items have been neglected may not necessarily help users minimize if they experience attachment to their clothing items (which is particularly common among hoarders). StoryLamp was built in response to this feedback.</p>
+		<br>
 		<font align = "center">
 		<img class="displayed" src="images/storylamp.jpg" align="middle">
 		</font>
 		<br>
+		<br>
 		<p>As Joshua Fields Millburn of The Minimalists writes, "An item that is <i>sentimental</i> for us can be <i>useful</i> for someone else."</p>
-		<p>StoryLamp is designed to feature dynamic images associated with the root of the discarded items&#39; sentimentality, which enables users to preserve memories and retain the identity those items bestowed upon them. RFID technology enables the lamp to glow when one of the user&#39;s donated items is being worn, positively reinforcing the user&#39;s decision to part with the item.</p>
+		<p>StoryLamp is designed to feature dynamic images associated with the root of the discarded items&#39; sentimentality, which allows users to preserve memories and retain the identity those items bestowed upon them. RFID technology enables the lamp to glow when one of the user&#39;s donated items is being worn, positively reinforcing the user&#39;s decision to part with the item.</p>
 
 		
 
